2c5503f6f4f858956cef7773eeeca8a6f3821cd0
EgoshinVA <egoshinvit@gmail.com>
2024-09-19 09:26:52 +0700

    Add global styled, theme component

---
+++ src/style/Theme.tsx
@@ -0,0 +1,9 @@
+export const theme = {
+    colors: {
+        primaryBg: '#202020',
+        secondaryBg: '#323232B2',
+        font: '#DEDEDE',
+        secondFont: '#959595',
+        button: '#9D9D9D'
+    }
+}
