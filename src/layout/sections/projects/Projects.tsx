e948713014950266240d9672f53baf79fced6348
EgoshinVA <egoshinvit@gmail.com>
2024-09-22 12:54:25 +0700

    Styling projects

--- src/layout/sections/projects/Projects.tsx
+++ src/layout/sections/projects/Projects.tsx
@@ -5,16 +5,19 @@ import figma from '../../../assets/images/figma.png'
 import bust from '../../../assets/images/bust.png'
 import hand from '../../../assets/images/hand.png'
 import {SectionTitle} from "../../../components/SectionTitle";
+import {Container} from "../../../components/Container";
 
 export const Projects = () => {
     return (
         <StyledProjects>
-            <SectionTitle>Personal Projects</SectionTitle>
-            <FlexWrapper>
-                <ProjectPhoto src={figma}/>
-                <ProjectPhoto src={bust}/>
-                <ProjectPhoto src={hand}/>
-            </FlexWrapper>
+            <Container>
+                <SectionTitle>Personal Projects</SectionTitle>
+                <FlexWrapper justify={'space-between'}>
+                    <ProjectPhoto src={figma}/>
+                    <ProjectPhoto src={bust}/>
+                    <ProjectPhoto src={hand}/>
+                </FlexWrapper>
+            </Container>
         </StyledProjects>
     );
 };
