58c4983a084832dfdfd0aa2d2ff37874bbab51dc
EgoshinVA <egoshinvit@gmail.com>
2024-09-26 12:40:58 +0700

    adaptive contact

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
             <Work/>
             <Visual/>
             <Projects/>
-            {/*<Contact/>*/}
+            <Contact/>
             {/*<Footer/>*/}
         </div>
     );
--- src/layout/sections/contact/Contact.tsx
+++ src/layout/sections/contact/Contact.tsx
@@ -3,14 +3,21 @@ import styled from "styled-components";
 import {SectionTitle} from "../../../components/SectionTitle";
 import {Button} from "../../../components/Button";
 import {theme} from "../../../style/Theme";
+import {font} from "../../../style/Common";
+import {Container} from "../../../components/Container";
 
 export const Contact = () => {
     return (
         <StyledContact>
+            <Container>
                 <SectionTitle>Contact Me</SectionTitle>
-                <Title><p>If you are looking to hire a product designer,</p>
-                    <p>I’m currently available for freelance work</p></Title>
+                <TitleWrapper>
+                    <Title>If you are looking to hire a product designer,
+                        I’m currently available for freelance work</Title>
+                </TitleWrapper>
+
                 <Button></Button>
+            </Container>
         </StyledContact>
     );
 };
@@ -19,17 +26,28 @@ const StyledContact = styled.section`
   button {
     margin: 0 auto;
   }
+
   padding-bottom: 124px;
-  
+
+`
+
+const TitleWrapper = styled.div`
+  display: flex;
+  justify-content: center;
 `
 
 const Title = styled.p`
+  ${font({weight: 600, Fmax: 26, Fmin: 18})}
   color: ${theme.colors.secondFont};
-  font-size: 26px;
-  font-weight: 600;
   text-align: center;
   margin-bottom: 48px;
-  
+  max-width: 600px;
+
+
+  @media ${theme.media.tablet} {
+    margin-bottom: 38px;
+  }
+
 `
 
 
